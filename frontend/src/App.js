b9a3034c15106bb7a9274bf8ff35c4c2b5c19cc3
Nassim <bellarabnassim@gmail.com>
2025-06-20 23:54:06 +0100

    feat: configure temporary routing between list and form components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import AddTask from "./components/Form/AddTask";
+import EditTask from "./components/Form/EditTask ";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoApp from "./components/TodoList/TodoList";
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TodoApp />} />
+          <Route path="/add" element={<AddTask />} />
+          <Route path="/edit/:id" element={<EditTask />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./Css/index.css";
+import "./assets/index.css";
 import App from "./App";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Optional for Bootstrap 5 icons
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
