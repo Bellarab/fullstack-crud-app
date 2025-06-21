97fa1feb88f827aea21c4e6e261817ef2aaf6fcf
Nassim <bellarabnassim@gmail.com>
2025-06-21 14:35:37 +0100

    feat: add AuthenticationController, JwtAuthenticationFilter, and AuthenticationResponse

---
+++ backend/src/main/java/com/nassim/backend/controller/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.nassim.backend.controller;
+
+import com.nassim.backend.model.AuthenticationResponse;
+import com.nassim.backend.model.User;
+import com.nassim.backend.service.impl.AuthenticationServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationServiceImpl authService;
+
+    public AuthenticationController(AuthenticationServiceImpl authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/refresh_token")
+    public ResponseEntity refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        return authService.refreshToken(request, response);
+    }
+}
