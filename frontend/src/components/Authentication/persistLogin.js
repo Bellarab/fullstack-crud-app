1e7bf722c21180731a0e9f1051e5e6202122610a
Nassim <bellarabnassim@gmail.com>
2025-06-24 15:38:40 +0100

    fix : fixing presist loging

--- frontend/src/components/Authentication/persistLogin.js
+++ frontend/src/components/Authentication/persistLogin.js
@@ -1,4 +1,68 @@
-test("renders input and add button", () => {
-  expect(screen.getByPlaceholderText(/add new task/i)).toBeInTheDocument();
-  expect(screen.getByRole("button", { name: /add task/i })).toBeInTheDocument();
-});
+import { Outlet } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+import UseRefreshToken from "../Hooks/UseRefreshToken";
+import useAuth from "../Hooks/UseAuth";
+import { useNavigate } from "react-router-dom";
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = UseRefreshToken();
+  const { auth, setAuth, persist } = useAuth();
+  const hasRefreshed = useRef(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    let isMounted = true;
+
+    const verifyRefreshToken = async () => {
+      if (hasRefreshed.current) return;
+      hasRefreshed.current = true;
+
+      try {
+        console.log("Refreshing token...");
+        const newAccessToken = await refresh();
+
+        if (!newAccessToken) {
+          console.warn("No new access token received. Clearing auth.");
+          setAuth({});
+        } else {
+          console.log("Token refreshed:", newAccessToken);
+        }
+      } catch (err) {
+        console.error("Token refresh failed:", err);
+        setAuth({});
+      } finally {
+        if (isMounted) setIsLoading(false);
+      }
+    };
+
+    if (!auth?.access_token && persist) {
+      verifyRefreshToken();
+    } else {
+      setIsLoading(false);
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, [auth?.access_token, persist, refresh, setAuth]);
+
+  return (
+    <>
+      {!persist ? (
+        <Outlet />
+      ) : isLoading ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="spinner-border " role="status">
+            <span className="sr-only"></span>
+          </div>
+        </div>
+      ) : (
+        <Outlet />
+      )}
+    </>
+  );
+};
+
+export default PersistLogin;
